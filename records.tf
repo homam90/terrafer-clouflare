da7df08e753155b44bd83bce6512c60768a67da8
alaa <alaa@example.com>
2025-08-25 12:08:39 +0300

    Update Cloudflare DNS record

--- records.tf
+++ records.tf
@@ -5,12 +5,4 @@ resource "cloudflare_record" "homam" {
   type    = "A"
   proxied = false
 }
-resource "cloudflare_record" "test" {
-  zone_id = data.cloudflare_zone.otazio-contracting_zone.id
-  name    = "test"
-  value   = "5.7.22.5"
-  type    = "A"
-  proxied = false
-
-}
 
