7a309d40ebcbb8ad3ee9584bb348292736f6b3c6
alaa <alaa@example.com>
2025-08-25 15:37:59 +0300

    Update test  DNS record

--- records.tf
+++ records.tf
@@ -5,4 +5,11 @@ resource "cloudflare_record" "homam" {
   type    = "A"
   proxied = false
 }
+resource "cloudflare_record" "ho" {
+  zone_id = data.cloudflare_zone.otazio-contracting_zone.id
+  name    = "homam"
+  value   = "5.14.14.5"
+  type    = "A"
+  proxied = false
+}
 
