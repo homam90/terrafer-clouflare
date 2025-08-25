d18eec8c9941d3ed527f00e7805b07601aba1718
homam90 <homamshm@gmail.com>
2025-08-25 10:11:18 +0300

    Update records.tf

--- records.tf
+++ records.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_record" "homam" {
-  zone_id = cloudflare_zone.otazio-contracting_zone.id
+  zone_id = data.cloudflare_zone.otazio-contracting_zone.id
   name    = "homam"
   value   = "5.6.5.5"
   type    = "A"
